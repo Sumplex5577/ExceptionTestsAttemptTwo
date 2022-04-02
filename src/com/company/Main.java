6c25a45babc066ef191cfacd0b8350d8d89b0a8c
Alex Krasnov <sashakrasnov12345@gmail.com>
2022-04-02 11:59:48 +0300

    try, catch and RunTimeException

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -196,40 +196,164 @@ public class Main {
 //
 //    }
 //}
-    public static void main(String[] args) {
-        System.err.println("#1.in");
-        f(); // создаем фрейм, помещаем в стек, передаем в него управление
-        System.err.println("#1.out"); // вернулись
-    } // выходим из текущего фрейма, кончились инструкции
-
-    public static void f() {
-        System.err.println(".   #2.in");
-        g(); // создаем фрейм, помещаем в стек, передаем в него управление
-        System.err.println(".   #2.out");  //вернулись
-    } // выходим из текущего фрейма, кончились инструкции
-
-    public static void g() {
-        System.err.println(".   .   #3.in");
-        h(); // создаем фрейм, помещаем в стек, передаем в него управление
-        System.err.println(".   .   #3.out"); // вернулись
-    } // выходим из текущего фрейма, кончились инструкции
-
-    public static void h() {
-        System.err.println(".   .   .   #4.in");
-        if (true) {
-            System.err.println(".   .   .   #4.RETURN");
-            return; // выходим из текущего фрейма по 'return'
-        }
-        System.err.println(".   .   .   #4.out"); // ПРОПУСКАЕМ
-    }
-}
-
-
-
-
-
-
-
+//33    public static void main(String[] args) {
+//        System.err.println("#1.in");
+//        f(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        System.err.println("#1.out"); // вернулись
+//    } // выходим из текущего фрейма, кончились инструкции
+//
+//    public static void f() {
+//        System.err.println(".   #2.in");
+//        g(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        System.err.println(".   #2.out");  //вернулись
+//    } // выходим из текущего фрейма, кончились инструкции
+//
+//    public static void g() {
+//        System.err.println(".   .   #3.in");
+//        h(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        System.err.println(".   .   #3.out"); // вернулись
+//    } // выходим из текущего фрейма, кончились инструкции
+//
+//    public static void h() {
+//        System.err.println(".   .   .   #4.in");
+//        if (true) {
+//            System.err.println(".   .   .   #4.RETURN");
+//            return; // выходим из текущего фрейма по 'return'
+//        }
+//        System.err.println(".   .   .   #4.out"); // ПРОПУСКАЕМ
+//    }
+//}
+//34    public static void main(String[] args) {
+//        System.err.println("#1.in");
+//        f(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        System.err.println("#1.out"); // ПРОПУСТИЛИ!
+//    }
+//
+//    public static void f() {
+//        System.err.println(".   #2.in");
+//        g(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        System.err.println(".   #2.out"); // ПРОПУСТИЛИ!
+//    }
+//
+//    public static void g() {
+//        System.err.println(".   .   #3.in");
+//        h(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        System.err.println(".   .   #3.out"); // ПРОПУСТИЛИ!
+//    }
+//
+//    public static void h() {
+//        System.err.println(".   .   .   #4.in");
+//        if (true) {
+//            System.err.println(".   .   .   #4.THROW");
+//            throw new Error(); // выходим со всей пачки фреймов ("раскрутка стека") по 'throw'
+//        }
+//        System.err.println(".   .   .   #4.out"); // ПРОПУСТИЛИ!
+//    }
+//}
+//35    public static void main(String[] args) {
+//        System.err.println("#1.in");
+//        try {
+//            f(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        } catch (Error e) { // "перехватили" "летящее" исключение
+//            System.err.println("#1.CATCH");  // и работаем
+//        }
+//        System.err.println("#1.out");  // работаем дальше
+//    }
+//
+//    public static void f() {
+//        System.err.println(".   #2.in");
+//        g(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        System.err.println(".   #2.out"); // ПРОПУСТИЛИ!
+//    }
+//
+//    public static void g() {
+//        System.err.println(".   .   #3.in");
+//        h(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        System.err.println(".   .   #3.out"); // ПРОПУСТИЛИ!
+//    }
+//
+//    public static void h() {
+//        System.err.println(".   .   .   #4.in");
+//        if (true) {
+//            System.err.println(".   .   .   #4.THROW");
+//            throw new Error(); // выходим со всей пачки фреймов ("раскрутка стека") по 'throw'
+//        }
+//        System.err.println(".   .   .   #4.out"); // ПРОПУСТИЛИ!
+//    }
+//}
+// 36   public static void main(String[] args) {
+//        System.err.println("#1.in");
+//        f(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        System.err.println("#1.out"); // вернулись и работаем
+//    }
+//
+//    public static void f() {
+//        System.err.println(".   #2.in");
+//        try {
+//            g(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        } catch (Error e) { // "перехватили" "летящее" исключение
+//            System.err.println(".   #2.CATCH");  // и работаем
+//        }
+//        System.err.println(".   #2.out");  // работаем дальше
+//    }
+//
+//    public static void g() {
+//        System.err.println(".   .   #3.in");
+//        h(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        System.err.println(".   .   #3.out"); // ПРОПУСТИЛИ!
+//    }
+//
+//    public static void h() {
+//        System.err.println(".   .   .   #4.in");
+//        if (true) {
+//            System.err.println(".   .   .   #4.THROW");
+//            throw new Error(); // выходим со всей пачки фреймов ("раскрутка стека") по 'throw'
+//        }
+//        System.err.println(".   .   .   #4.out"); // ПРОПУСТИЛИ!
+//    }
+//}
+// 37   public static void main(String[] args) {
+//        System.err.println("#1.in");
+//        f(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        System.err.println("#1.out"); // вернулись и работаем
+//    }
+//
+//    public static void f() {
+//        System.err.println(".   #2.in");
+//        g(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        System.err.println(".   #2.out"); // вернулись и работаем
+//    }
+//
+//    public static void g() {
+//        System.err.println(".   .   #3.in");
+//        try {
+//            h(); // создаем фрейм, помещаем в стек, передаем в него управление
+//        } catch (Error e) { // "перехватили" "летящее" исключение
+//            System.err.println(".   .   #3.CATCH");  // и работаем
+//        }
+//        System.err.println(".   .   #3.out");  // работаем дальше
+//    }
+//
+//    public static void h() {
+//        System.err.println(".   .   .   #4.in");
+//        if (true) {
+//            System.err.println(".   .   .   #4.THROW");
+//            throw new Error(); // выходим со всей пачки фреймов ("раскрутка стека") по 'throw'
+//        }
+//        System.err.println(".   .   .   #4.out"); // ПРОПУСТИЛИ!
+//    }
+//}
+//    public static void main(String[] args) {
+//        try {
+//            System.err.print(" 0");
+//            if (true) {throw new RuntimeException();}
+//            System.err.print(" 1");
+//        } catch (Exception e) { // catch по Exception ПЕРЕХВАТЫВАЕТ RuntimeException
+//            System.err.print(" 2");
+//        }
+//        System.err.println(" 3");
+//    }
+//}
 
 
 
