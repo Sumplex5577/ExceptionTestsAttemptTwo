241e74e24115fadcca9398d5b218bac562ace0c9
Alex Krasnov <sashakrasnov12345@gmail.com>
2022-04-02 11:39:24 +0300

    Throw New RuntimeException

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
 public class Main {
-
     //1    public static void main(String[] args) throws Throwable {
 //2  public static void main(String[] args) throws String {
 //3 public static void main(String[] args) {
@@ -108,3 +107,30 @@ public class Main {
 //        }
 //    }
 //}
+// 22    public static double sqr(double arg) {
+//        throw new RuntimeException();
+//    }
+//}
+// 23  public static double sqr(double arg) {// согласно объявлению метода ты должен вернуть double
+//        long time = System.currentTimeMillis();
+//        if (time % 2 == 0) {
+//            return arg * arg;             // ок, вот твой double
+//        } else if (time % 2 == 1) {
+//            {
+//                while (true) ;                 // не, я решил "повиснуть"
+//            } else{
+//                throw new RuntimeException(); // или бросить исключение
+//            }
+//        }
+//    }
+//}
+    public static void main(String[] args) {
+        // sqr - "сломается" (из него "выскочит" исключение),
+        double d = sqr(10.0);  // выполнение метода main() прервется в этой строчке и
+        // d - НИКОГДА НИЧЕГО НЕ БУДЕТ ПРИСВОЕНО!
+        System.out.println(d); // и печатать нам ничего не придется!
+    }
+    public static double sqr(double arg) {
+        throw new RuntimeException(); // "бросаем" исключение
+    }
+}
