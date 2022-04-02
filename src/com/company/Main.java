2356271fcc78560e3ba65196b2159c6ab8c02158
Alex Krasnov <sashakrasnov12345@gmail.com>
2022-04-02 11:34:22 +0300

    While true loading forever

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -80,11 +80,15 @@ public class Main {
 //        // а если нечетное, что нам возвращать?
 //    }
 //}
-    public static void main(String[] args) {
-        double d = sqr(10.0); // ну, и чему равно d?
-        System.out.println(d);
-    }
+// 18  public static void main(String[] args) {
+//        double d = sqr(10.0); // ну, и чему равно d?
+//        System.out.println(d);
+//    }
+//    public static double sqr(double arg) {
+//        // nothing
+//    }
+//}
     public static double sqr(double arg) {
-        // nothing
+        while (true); // Удивительно, но КОМПИЛИРУЕТСЯ!
     }
 }
