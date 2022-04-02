06d28642a9acfde39cbdf274435531fb7dc43548
Alex Krasnov <sashakrasnov12345@gmail.com>
2022-04-02 12:00:27 +0300

    Runtimeexception1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -354,7 +354,19 @@ public class Main {
 //        System.err.println(" 3");
 //    }
 //}
-
+    public static void main(String[] args) {
+        try {
+            throw new RuntimeException();
+        } catch (Exception e) {
+            if (e instanceof RuntimeException) {
+                RuntimeException re = (RuntimeException) e;
+                System.err.print("Это RuntimeException на самом деле!!!");
+            } else {
+                System.err.print("В каком смысле не RuntimeException???");
+            }
+        }
+    }
+}
 
 
 
