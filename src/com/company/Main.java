46fef5d56ef4793d442e8c00f43ebb6896d1a14d
Alex Krasnov <sashakrasnov12345@gmail.com>
2022-04-02 11:19:04 +0300

    Try, catch and (String s) doesnt work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,14 @@ package com.company;
 
 public class Main {
 
-     // public static void main(String[] args) throws Throwable {
-    //  public static void main(String[] args) throws String {
+     //1 public static void main(String[] args) throws Throwable {
+     //2  public static void main(String[] args) throws String {
+    //3 public static void main(String[] args) {
+    //3   try {
+    //3 } catch (Throwable t) {}
      public static void main(String[] args) {
          try {
-         } catch (Throwable t) {}
+         } catch (String s) {}
      }
 }
 
