94df6058b1a6f43c8cfb0b4f6569927efd9f47c1
Alex Krasnov <sashakrasnov12345@gmail.com>
2022-04-02 11:40:10 +0300

    Missing return statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -124,13 +124,21 @@ public class Main {
 //        }
 //    }
 //}
-    public static void main(String[] args) {
-        // sqr - "сломается" (из него "выскочит" исключение),
-        double d = sqr(10.0);  // выполнение метода main() прервется в этой строчке и
-        // d - НИКОГДА НИЧЕГО НЕ БУДЕТ ПРИСВОЕНО!
-        System.out.println(d); // и печатать нам ничего не придется!
-    }
-    public static double sqr(double arg) {
-        throw new RuntimeException(); // "бросаем" исключение
-    }
-}
+// 24  public static void main(String[] args) {
+//        // sqr - "сломается" (из него "выскочит" исключение),
+//        double d = sqr(10.0);  // выполнение метода main() прервется в этой строчке и
+//        // d - НИКОГДА НИЧЕГО НЕ БУДЕТ ПРИСВОЕНО!
+//        System.out.println(d); // и печатать нам ничего не придется!
+//    }
+//    public static double sqr(double arg) {
+//        throw new RuntimeException(); // "бросаем" исключение
+//    }
+//}
+//25    public static int area(int width, int height) {
+//        if (width < 0 || height < 0) {
+//            // у вас плохие аргументы, извините
+//        } else {
+//            return width * height;
+//        }
+//    }
+
