4c34476697b816e989b920fbb5364363fb3c50b3
Alex Krasnov <sashakrasnov12345@gmail.com>
2022-04-02 11:16:47 +0300

    Throws String doesnt work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,9 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) throws Throwable {
-        
+  //  public static void main(String[] args) throws Throwable {
+    public static void main(String[] args) throws String {
+
+
     }
 }
