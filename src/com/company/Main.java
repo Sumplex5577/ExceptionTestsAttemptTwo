7c9b8829adc85f8b3d9d16bcbd43c670133d0d6e
Alex Krasnov <sashakrasnov12345@gmail.com>
2022-04-02 11:17:46 +0300

    Try, catch and Throwable t

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,12 @@ package com.company;
 
 public class Main {
 
-  //  public static void main(String[] args) throws Throwable {
-    public static void main(String[] args) throws String {
-
-
-    }
+     // public static void main(String[] args) throws Throwable {
+    //  public static void main(String[] args) throws String {
+     public static void main(String[] args) {
+         try {
+         } catch (Throwable t) {}
+     }
 }
+
+
