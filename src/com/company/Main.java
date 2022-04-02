a43fa569c6af028ad6ad359a1d8ae3fa004706b7
Alex Krasnov <sashakrasnov12345@gmail.com>
2022-04-02 11:21:11 +0300

    Throw new Error

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,19 @@ package com.company;
 
 public class Main {
 
-     //1 public static void main(String[] args) throws Throwable {
-     //2  public static void main(String[] args) throws String {
-    //3 public static void main(String[] args) {
-    //3   try {
-    //3 } catch (Throwable t) {}
-     public static void main(String[] args) {
-         try {
-         } catch (String s) {}
-     }
+//1 public static void main(String[] args) throws Throwable {
+//2  public static void main(String[] args) throws String {
+//3 public static void main(String[] args) {
+//3   try {
+//3 } catch (Throwable t) {}
+//4public static void main(String[] args) {
+//4try {
+//4 } catch (String s) {}
+//4    }
+//4}
+public static void main(String[] args) {
+    // Error - потомок Throwable
+    throw new Error();
 }
-
+}
 
